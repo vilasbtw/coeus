460705ae1f8eb5cfb464f3a622b6dea7db36bbcd
vilasbtw <kaiquevilas@icloud.com>
2025-05-22 10:45:41 -0300

    Customizing Json responses

--- books/src/main/java/com/coeus/books/models/dtos/BookDTO.java
+++ books/src/main/java/com/coeus/books/models/dtos/BookDTO.java
@@ -5,12 +5,23 @@ package com.coeus.books.models.dtos;
 
 // To retrieve a value from a record class, we don't call getters, but the attributes instead, e.g.:
 // Instead of bookDTO.getId(), you should call bookDTO.id() only.
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"book_name", "author_name", "publisher_name", "price", "number_of_pages", "id"})
 public record BookDTO(
         Long id,
+        @JsonProperty("book_name")
         String bookName,
+        @JsonProperty("author_name")
         String authorName,
+        @JsonProperty("publisher_name")
         String publisherName,
+        @JsonProperty("number_of_pages")
         int numberOfPages,
+        @JsonIgnore
         String genre,
         double price
 ){}
