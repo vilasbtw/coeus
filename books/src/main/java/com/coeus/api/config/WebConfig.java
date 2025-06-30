e10ae8e3c9830af9a7144b1ee99117af413aa0ef
vilasbtw <kaiquevilas@icloud.com>
2025-06-30 17:32:35 -0300

    Add CORS Mappings

--- books/src/main/java/com/coeus/api/config/WebConfig.java
+++ books/src/main/java/com/coeus/api/config/WebConfig.java
@@ -3,27 +3,26 @@ package com.coeus.api.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// This class allows our API to produce and consume data in JSON, XML and YAML format.
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // Manage which domain or client can access our API.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // Enable CORS for the React frontend (localhost:3000)
+            .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    // Allows our API to produce and consume data in JSON, XML and YAML format.
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        // Via EXTENSION: localhost:8080/books/1.xml (DEPRECATED)
-        // Via QUERY PARAM: localhost:8080/books/1?mediaType=xml
-
-        /*
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
-                .useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
-        */
 
         // Via HEADER PARAM: localhost:8080/books/1
         configurer.favorParameter(false)
