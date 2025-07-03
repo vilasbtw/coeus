54602b018a1396521d075b6412d16195208cd62f
vilasbtw <kaiquevilas@icloud.com>
2025-07-03 18:15:10 -0300

    Adding student.create() Integration Test

--- books/src/test/java/com/coeus/api/config/TestConfigs.java
+++ books/src/test/java/com/coeus/api/config/TestConfigs.java
@@ -4,4 +4,11 @@ public interface TestConfigs {
 
     int SERVER_PORT = 8888;
 
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+
+    String AUTHORIZED_ORIGIN = "http://localhost:3000";
+    String UNATHOURIZED_ORIGIN = "http://localhost:1234";
+
 }
---
+++ books/src/test/java/com/coeus/api/integrationtests/controllers/withjson/StudentControllerTest.java
@@ -0,0 +1,105 @@
+package com.coeus.api.integrationtests.controllers.withjson;
+
+import com.coeus.api.config.TestConfigs;
+import com.coeus.api.integrationtests.dto.StudentDTO;
+import com.coeus.api.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class StudentControllerTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+    private static StudentDTO studentDTO;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+        // ignores HATEOAS links.
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        studentDTO = new StudentDTO();
+    }
+
+    @Test
+    @Order(1)
+    void create() throws JsonProcessingException {
+        mockStudent();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.AUTHORIZED_ORIGIN)
+                .setBasePath("/students")
+                .setPort(TestConfigs.SERVER_PORT)
+                .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                .build();
+
+        var content = given(specification)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(studentDTO)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        // rest-assured uses its own object mapper for serialization, which may cause problems.
+        // to avoid that, we are deserializing the response manually.
+        StudentDTO createdDTO = objectMapper.readValue(content, StudentDTO.class);
+        studentDTO = createdDTO;
+
+        assertNotNull(createdDTO.getId());
+        assertNotNull(createdDTO.getStudentRegister());
+        assertNotNull(createdDTO.getName());
+        assertNotNull(createdDTO.getEmail());
+        assertNotNull(createdDTO.getCourse());
+
+        assertTrue(createdDTO.getId() > 0);
+
+        assertEquals("HT3036901", createdDTO.getStudentRegister());
+        assertEquals("Kaique", createdDTO.getName());
+        assertEquals("kaique@gmail.com", createdDTO.getEmail());
+        assertEquals("System Development And Analysis", createdDTO.getCourse());
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void mockStudent() {
+        studentDTO.setStudentRegister("HT3036901");
+        studentDTO.setName("Kaique");
+        studentDTO.setEmail("kaique@gmail.com");
+        studentDTO.setCourse("System Development And Analysis");
+    }
+
+}
